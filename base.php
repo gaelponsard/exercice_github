e34fe384e663f73b54bca2c6f1c35af9a6862779
1 <g.ponsard@simplon-charleville.fr>
2019-04-03 14:41:10 +0200

    ajout section 1

--- base.php
+++ base.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
 <html lang="fr">
-	<head>
-	  <meta charset="UTF-8">
-	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-	  <title>Exercice Github</title>
-	  <link rel="stylesheet" type="text/css" href="style.css">
-	</head>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-	<body>
-	 <header>
-	 </header>
-	  <script src=" "></script>
-	</body>
+	<title>Exercice Github</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<!-- header fixé et de 180 pixels de haut -->
+	<header>
+	</header>
+
+<!-- section 1 de 800 pixels de haut -->
+	<div class="section1"><p>test</p>
+	</div>
+
+
+	<script src=" "></script>
+</body>
+
 </html>
