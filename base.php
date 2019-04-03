257fd6c78ea6b818f8578d280c4b5225dec850e2
1 <g.ponsard@simplon-charleville.fr>
2019-04-03 14:22:20 +0200

    header

--- base.php
+++ base.php
@@ -5,12 +5,14 @@
 	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	  <title>Document</title>
-	  <link rel="stylesheet" type="text/css" href=" ">
+
+	  <title>Exercice Github</title>
+	  <link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 
 	<body>
-	 
+	 <header>
+	 </header>
 	  <script src=" "></script>
 	</body>
 </html>
